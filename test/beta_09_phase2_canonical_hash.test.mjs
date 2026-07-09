5a898ea62551e03a6a93fffeab217c33c5abcc6e
robertsc2049-bit <robertsc2049@gmail.com>
2026-07-09 18:19:53 +0100

    BETA-09 phase2 canonical hash

--- test/beta_09_phase2_canonical_hash.test.mjs
+++ test/beta_09_phase2_canonical_hash.test.mjs
@@ -3,7 +3,7 @@ import crypto from "node:crypto";
 import fs from "node:fs/promises";
 import path from "node:path";
 import test from "node:test";
-import ts from "typescript";
+import * as ts from "typescript";
 
 let phase2Promise = null;
 
